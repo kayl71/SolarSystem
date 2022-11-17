d974442ed79edf59907512e361dcac97145dc2b5
Mmvbr <vormax3@gmail.com>
2022-11-18 01:04:48 +0300

    321

--- solar_model.py
+++ solar_model.py
@@ -40,12 +40,13 @@ def move_space_object(body, dt):
     """
     
     ax = body.Fx/body.m
-    body.x += body.Vx*dt
+    
     body.Vx+=ax*dt
+    body.x += body.Vx*dt
     ay = body.Fy/body.m
-    body.y += body.Vy*dt
+    
     body.Vy += ay*dt
-
+    body.y += body.Vy*dt
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
